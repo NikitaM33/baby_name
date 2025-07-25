26d81f87545d25344fe60ac005b75df9dd68468e
Никита Мельченков <nikitam33@hotmail.com>
2025-07-25 17:49:27 +0300

    init

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="libs/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/main.css">
+    <title>Baby name</title>
+</head>
+
+<body>
+    <div class="header">
+        <div class="name">
+            <h1>
+                <a href="#">Baby name</a>
+            </h1>
+        </div>
+    </div>
+
+    <div class="menu">
+        <div class="menu__buttons">
+            <button class="menuBtn male">Для мальчиков</button>
+            <button class="menuBtn female">Для девочек</button>
+        </div>
+    </div>
+
+    <section>
+        <div class="banner">
+            <div class="mom">
+                <img src="./assets/mom-PIMPED2.png" alt="mom">
+            </div>
+            <div class="banner__tagline">
+                <p>Подберите своему ребенку особенное имя</p>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ pages/male.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="../libs/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="../styles/pages/male.css">
+    <title>Имена для мальчиков</title>
+</head>
+
+<body>
+    <div class="header">
+        <div class="name">
+            <h1>
+                <a href="#">Baby name</a>
+            </h1>
+        </div>
+
+        <div class="header__text">
+            <h3>Имена для мальчиков</h3>
+        </div>
+    </div>
+
+    <section class="banner-section">
+        <div class="banner">
+            <div class="baby">
+                <img src="../assets/babyBoy-PIMPED1.png" alt="baby boy">
+            </div>
+        </div>
+
+        <div class="random-name">
+            <h2>Александр</h2>
+            <div class="random-name__buttons">
+                <button class="random-name__button">Все имена</button>
+                <button class="random-name__button">Другое имя</button>
+            </div>
+        </div>
+    </section>
+
+    <section class="origin-section">
+        <div class="origin-name">
+            <p>Происхождение имени</p>
+            <div class="origin-name__chevron">
+                <img src="../assets/chevron.png" alt="Chevron">
+            </div>
+        </div>
+
+        <div class="origin-tale">
+            <p>
+                Имя Александр имеет древнегреческое происхождение и является одним из самых популярных и
+                распространённых имён в мире, в том числе и в России. Его происхождение связано с именем «Александрос»
+                (Αλέξανδρος), которое состоит из двух частей:
+                «Але́ксо» (αλέξω) — означает «защищать», «оборонять», «отважно защищать».
+                «Андрос» (ανδρός) — означает «мужчина», «муж».
+                Таким образом, имя Александр можно перевести как «Защитник людей» или «Обороняющий людей».
+                Историческая значимость имени связана с великим полководцем и царём Александром Македонским (356–323 гг.
+                до н.э.), который создал одну из крупнейших империй древнего мира. Его имя стало символом силы,
+                лидерства и мужества.
+                В христианской традиции имя Александр приобрело популярность благодаря святым и мученикам с этим именем,
+                что способствовало его распространению в православных странах, включая Россию.
+                В целом, имя Александр символизирует защиту, силу и лидерство, и остаётся популярным на протяжении
+                многих веков.
+            </p>
+        </div>
+    </section>
+
+
+</body>
+
+</html>
