53873d82231b20bc939c0dc6aa05f0e4c3658260
Никита Мельченков <nikitam33@hotmail.com>
2025-09-24 22:12:16 +0300

    add name storage

--- src/js/maleNames.js
+++ src/js/maleNames.js
@@ -1,7 +1,20 @@
-import {getNames} from './randomName.js';
+import { getNames } from './randomName.js';
+
+let anotherMaleName = document.querySelector('#anotherMaleName');
+let randName = document.querySelector('.randName');
 
 window.addEventListener('load', async function () {
-  let maleNames = await getNames('Male');
-
-  console.log(maleNames);
+  await getNames('Male');
 });
+
+let randomName = () => {
+  if (localStorage.getItem('names')) {
+    let maleNames = JSON.parse(localStorage.getItem('names'));
+    let randomNum = Math.floor(Math.random() * maleNames.length) + 1;
+
+    randName.innerText = maleNames[randomNum].name;
+  }
+};
+
+anotherMaleName.addEventListener('click', randomName);
+
--- src/js/randomName.js
+++ src/js/randomName.js
@@ -1,16 +1,16 @@
-let names = [];
 let randName = document.querySelector('.randName');
 
 export let getNames = async (gender) => {
   try {
     let response = await fetch(`/src/helpers/api${gender}.php`);
-    names = await response.json();
+    let names = await response.json();
 
     if (response.ok) {
       let randomNum = Math.floor(Math.random() * names.length) + 1;
 
       randName.innerText = names[randomNum].name;
-      return names;
+      localStorage.removeItem('names');
+      localStorage.setItem('names', JSON.stringify(names));
     }
   } catch (error) {
     console.error('Не удалось получить ответ от БД. ', error);
